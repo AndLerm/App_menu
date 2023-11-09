c44b4edbfc8e9e7f679d79f6fa7b5ce4fe261b88
Bennylnz <bennylanza@gmail.com>
2023-11-09 13:01:17 +0100

    deploy

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+        firebaseConfig : {
+        apiKey: "AIzaSyDSTPXbx6fgL00tPUvk_i8llb5_H3m25sQ",
+        authDomain: "ristorante-sulmare-c9184.firebaseapp.com",
+        databaseURL: "https://ristorante-sulmare-c9184-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "ristorante-sulmare-c9184",
+        storageBucket: "ristorante-sulmare-c9184.appspot.com",
+        messagingSenderId: "133084637748",
+        appId: "1:133084637748:web:a30d52aa75c7dc86187861"
+      }
+}
